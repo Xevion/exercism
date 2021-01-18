15e89a76537a7b2a5ec6df708231430305036f6b
Xevion <xevioni@yandex.com>
2021-01-18 02:55:58 -0600

    word count elixir

---
+++ elixir/word-count/lib/word_count.ex
@@ -0,0 +1,17 @@
+defmodule WordCount do
+  @splitters String.graphemes(" _!&@:;'\"[]()_=+,$&%^")
+
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.split(@splitters)
+    |> Enum.filter(&(String.length(&1) > 0))
+    |> Enum.reduce(%{}, fn (word, count) -> Map.update(count, word, 1, &(&1 + 1)) end)
+  end
+end
