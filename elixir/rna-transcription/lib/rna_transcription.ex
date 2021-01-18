4c28e4d439898a99d9d47144e91058b15d33b213
Xevion <xevioni@yandex.com>
2021-01-18 01:15:35 -0600

    add hello world properly, add rna-transcription (elixir is neat!)

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,17 @@
+defmodule RnaTranscription do
+  # Turns all 1-item charlist keys and values inside the base map into integers
+  @transcription %{'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'} |> Enum.map(fn ({k, v}) -> {List.first(k), List.first(v)} end) |> Enum.into(%{})
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna |> Enum.map(fn (x) -> Map.get(@transcription, x) end)
+  end
+end
