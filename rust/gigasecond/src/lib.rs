532545e592c0220703621ec94421194a88dbfe59
Xevion <xevion@xevion.dev>
2023-07-01 15:43:37 -0500

    Begin Rust track : hello-world, gigasecond

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,10 @@
+use std::time::SystemTime;
+
+use time::PrimitiveDateTime as DateTime;
+
+const GIGASECOND_IN_SECONDS: i64 = 1_000_000_000;
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start + time::Duration::seconds(GIGASECOND_IN_SECONDS)
+}
